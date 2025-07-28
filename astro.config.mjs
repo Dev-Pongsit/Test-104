201c24de2418a8867541a2bac3da87e04c5fc0f8
[Dev] Pongisit <illslic47@gmail.com>
2025-07-28 17:57:35 +0700

    edit img2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  base: '/Test-104/' // เปลี่ยนให้ตรงกับชื่อ repository เช่น '/Test-104/'
+  base: '/Test-104/'
 });
